0e4ba2e5f63bc406d06b1c3b25c2ba3b773156ae
JasonQ1232 <47706293+JasonQ1232@users.noreply.github.com>
2022-08-08 21:20:47 +0100

    s

--- starlight-transmuter/script.lua
+++ starlight-transmuter/script.lua
@@ -3,18 +3,23 @@ local component = require("component")
 
 local settings = dofile("/usr/bin/starlight-transmuter/settings.cfg")
 
-local rs = component.proxy(component.get(settings.redstone_address))
-local geo = component.proxy(component.get(settings.geolyzer_address))
-
 while true do
-    result_table = geo.analyze(settings.geolyzer_side)
-    for block in pairs(settings.whitelist) do
-        block_name = settings.whitelist[block]
-        if(result_table.name == block_name) then
-            rs.setOutput(settings.redstone_side, 15)
-            rs.setOutput(settings.redstone_side, 0)
+    for index in pairs(settings.rigs) do
+        local redstone = component.proxy(component.get(settings.rigs[index].redstone_address))
+        local redstone_side = component.proxy(component.get(settings.rigs[index].redstone_side))
+        local geolyzer = component.proxy(component.get(settings.rigs[index].geolyzer_address))
+        local geolyzer_side = component.proxy(component.get(settings.rigs[index].geolyzer_side))
+    
+        local result_table = geolyzer.analyze(geolyzer_side)
+        if (result_table.name ~= "minecraft:air") then
+            for block in pairs(settings.whitelist) do
+                if (result_table.name == settings.whitelist[block]) then
+                    redstone.setOuput(redstone_side, 15)
+                    redstone.setOutput(redstone_side, 0)
+                end
+            end
         end
+        os.sleep(0.1)
     end
-    os.sleep(0.2)
 end
 
