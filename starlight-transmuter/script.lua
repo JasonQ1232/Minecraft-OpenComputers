41898475131343662700c92472b57c0ef17ee16d
JasonQ1232 <47706293+JasonQ1232@users.noreply.github.com>
2022-08-08 03:56:36 +0100

    s

--- starlight-transmuter/script.lua
+++ starlight-transmuter/script.lua
@@ -1,34 +1,15 @@
 local os = require("os")
 local component = require("component") 
-local sides = require("sides")
 
 local settings = dofile("/usr/bin/starlight-transmuter/settings.cfg")
 
 local rs = component.proxy(component.get(settings.redstone_address))
 local geo = component.proxy(component.get(settings.geolyzer_address))
 
-local whitelist = {
-    "gregtech:ore_uranium_0", 
-    "minecraft:clay", 
-    "minecraft:end_stone", 
-    "gregtech:compressed_7", 
-    "contenttweaker:sub_block_holder_0",
-    "contenttweaker:sub_block_holder_1",
-    "contenttweaker:sub_block_holder_3",
-    "contenttweaker:sub_block_holder_4",
-    "contenttweaker:sub_block_holder_5",
-    "contenttweaker:sub_block_holder_6",
-    "actuallyadditions:block_crystal_cluster_redstone",
-    "actuallyadditions:block_crystal_cluster_lapis",
-    "actuallyadditions:block_crystal_cluster_coal",
-    "actuallyadditions:block_crystal_cluster_diamond",
-    "actuallyadditions:block_crystal_cluster_iron"
-}
-
 while true do
-    result_table = geo.analyze(settings.geolyzer-side)
-    for block in pairs(whitelist) do
-        block_name = whitelist[block]
+    result_table = geo.analyze(settings.geolyzer_side)
+    for block in pairs(settings.whitelist) do
+        block_name = settings.whitelist[block]
         if(result_table.name == block_name) then
             rs.setOutput(settings.redstone_side, 15)
             rs.setOutput(settings.redstone_side, 0)
