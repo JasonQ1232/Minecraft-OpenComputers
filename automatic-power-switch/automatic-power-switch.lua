17cd464e598eb3f23a5f28adce58154811f5ce8e
JasonQ1232 <47706293+JasonQ1232@users.noreply.github.com>
2022-08-08 05:46:27 +0100

    s

--- automatic-power-switch/automatic-power-switch.lua
+++ automatic-power-switch/automatic-power-switch.lua
@@ -4,7 +4,7 @@ local thread = require "thread"
 
 local threads = {}
 local program = "/usr/bin/automatic-power-switch/script.lua"
-local readout = "/usr/bin/automatic-power-switch/readout.lua"
+local program_readout = "/usr/bin/automatic-power-switch/readout.lua"
 
 function start()
     local proc = thread.create(shell.execute, program, os.getenv("shell"))
@@ -18,4 +18,10 @@ function stop()
             proc:kill()
         end
     end
+end
+
+function readout()
+    local proc = thread.create(shell.execute, program_readout, os.getenv("shell"))
+    proc:detach()
+    table.insert(threads, proc)
 end
